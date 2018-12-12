d1d52313fb3edbfeed4e6172c2ec1c50fe6b62b8
Sanjay Mangrani <sanjaykumar22691@gmail.com>
2018-12-13 01:43:25 +0800

    My Projet Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/net/prac/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.prac.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
